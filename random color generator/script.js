8d307ae11adf06143f2b80b46ed11c59c0ba34e5
Kan Halder <59249490+KanLSK@users.noreply.github.com>
2022-06-22 17:06:53 +0700

    Add files via upload

---
+++ random color generator/script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const body = document.querySelector('body');
+const tileLeft = document.querySelector('.tile-left');
+const tileRight = document.querySelector('.tile-right');
+
+function randomNumber(number) {
+    return Math.floor(Math.random() * number)
+}
+
+function randomColor() {
+    let bgColor = `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`
+    return bgColor
+}
+
+
+container.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = randomColor()
+    e.target.innerHTML = "<p>" + `${randomColor()}` + "</p>"
+})
